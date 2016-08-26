c93306b7c21bdf82df871af2eebd377adcf1f5fd
Margheriti <lilrom13@gmail.com>
2016-08-26 16:51:59 +0200

    adding twitter constants

---
+++ MyCanadianBlog/Constants/Twitter.swift
@@ -0,0 +1,16 @@
+//
+//  Twitter.swift
+//  MyCanadianBlog
+//
+//  Created by Romain Margheriti on 19/08/2016.
+//  Copyright © 2016 Romain Margheriti. All rights reserved.
+//
+
+import Foundation
+
+class TwitterConstants {
+    static let ConsumerKey = "nA3p6xb0tEFzcFmIPCRkErvk3"
+    static let ConsumerSecret = "CtYBpv3meE1yRlEFSVndT6KprD68halhUXP1lb9agQRwqEgS6Y"
+    static let AccessToken = "393215253-TxA5NSEeIwzVrA1MO1JeD3TUrWcJoAoSafRQhEiU"
+    static let AccessTokenSecret = "zuFqaOvRmkjMPbAvmzoA5lZFnJhNQTnQ2n0pXUGEJnbi4"
+}
